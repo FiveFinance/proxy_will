6798067e138cb417d16d64f770f59466c0d41fac
William Wong <williamwj.wong@gmail.com>
2019-04-09 20:03:26 -0700

    small fixes to the proxy server

--- server.js
+++ server.js
@@ -35,17 +35,17 @@ app.use('/api/history/:ticker', (req, res) => {
   .catch(err => res.send(err));
 });
 
-// app.use('/api/stocks/:ticker', (req, res) => {
-//   axios3002.get(`/api/stocks/${req.params.ticker}`)
-//   .then(response => res.send(response.data))
-//   .catch(err => res.send(err));
-// });
+app.use('/api/stocks/:ticker', (req, res) => {
+  axios3002.get(`/api/stocks/${req.params.ticker}`)
+  .then(response => res.send(response.data))
+  .catch(err => res.send(err));
+});
 
-// app.use('/api/accounts/:account_number', (req, res) => {
-//   axios3002.get(`/api/accounts/${req.params.account_number}`)
-//   .then(response => res.send(response.data))
-//   .catch(err => res.send(err));
-// });
+app.use('/api/accounts/:account_number', (req, res) => {
+  axios3002.get(`/api/accounts/${req.params.account_number}`)
+  .then(response => res.send(response.data))
+  .catch(err => res.send(err));
+});
 
 app.use('/api/quotes/:symbol', (req, res) => {
   axios3003.get(`/api/quotes/${req.params.symbol}`)
@@ -54,7 +54,7 @@ app.use('/api/quotes/:symbol', (req, res) => {
 })
 
 app.use('/stocks/tags/:tag', (req, res) => {
-  axios3003.get(`/api/stocks/tags/${req.params.tag}`)
+  axios3003.get(`/stocks/tags/${req.params.tag}`)
   .then(response => res.send(response.data))
   .catch(err => res.send(err))
 })
